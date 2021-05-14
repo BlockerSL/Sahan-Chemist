765295fc273f93ace6c9eef60bdfdd2d895a0b86
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-14 18:19:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
 function sahand()
 {
-  var yak = document.getElementById("yak").value;
+  var Yak = document.getElementById("yak").value;
   if (yak == "1"){
     console.log("1");
     
   }
   else{
     console.log("Erore")
-    return Tag;
+    return 
   }
     
+}
