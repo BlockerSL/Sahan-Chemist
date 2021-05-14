7fee6be01c12ba8cc43ec736deb9890c0e92df29
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-15 02:15:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-
-function sahand()
-{
-  var Yak = document.getElementById("yak").value;
-  if (yak == "1"){
-    console.log("1");
-    
-  }
-  else{
-    console.log("Erore");
-    
-  }
-    
-}
+    function sahand(){
+      var num1, num2, anjam;
+      num1 = NUMBER(document.form.value);
+      num2 = Number(document.form.num2.value);
+      anjam = num1+num2;
+      document.form.anjam.value=btn;
+    }
