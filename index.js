baff8d7282fed4d83695709a07b51dcbcb302a5a
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-14 18:02:20 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function sahand()
+{
+  var Tag = document.getElementById("yak").value;
+  if (yak == "1"){
+    console.log("1");
+    
+  }
+  else{
+    console.log("Erore")
+    return Tag;
+  }
+    
