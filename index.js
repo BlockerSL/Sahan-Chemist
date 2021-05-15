40eb2145bf583d062d1aa0a246404762147ce63a
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-15 21:44:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function sahand()
 {
   var num1 = document.getElementById("num1").value;
   var num2 = document.getElementById("num2").value;
-  var anjam = document.getElementById("anjam").value;
+ 
   
   if(num1 == "Cl"){
     document.write("Clored");
