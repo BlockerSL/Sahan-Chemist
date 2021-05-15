2b2bf24943b7ae3435b94dad2d548f872077d854
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-15 20:39:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-    function sahand(){
-      var num1, num2, anjam;
-      num1 = NUMBER(document.form.value);
-      num2 = Number(document.form.num2.value);
-      anjam = num1+num2;
-      document.form.anjam.value=btn;
-    }
+function sahand()
+{
+  var num1 = document.getElementById("num1").value;
+  var num2 = document.getElementById("num2").value;
+  var anjam = document.getElementById("anjam").value;
+  
+  if(num1 == "Cl"){
+    document.write("Clored");
+  }
+  else if (num2 = "Br"){
+    document.write("Bromed");
+  }
+  else {
+        document.write("Erore");
+
+    
+  }
+  anjam = document.write(num1+num2);
+}
