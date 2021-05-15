49b127015224b4742444df1ed45d2a5b72e5a8ed
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-15 23:02:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ var x = document.getElementById("Tag").value;
 
 if (x == "Cl") {
     window.location.href = 'sahand/index.html';
-}else {
+}
+if (x == "F"){
+
+window.location.href = 'sahand1/index.html';
+
+}
+else {
   document.write("Erore");
 }
 }
