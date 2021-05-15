da08f72a0ab150e614a7dd6674d9ad3c32d855c2
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-15 23:04:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var x = document.getElementById("Tag").value;
 if (x == "Cl") {
     window.location.href = 'sahand/index.html';
 }
-if (x == "F"){
+else if (x == "F"){
 
 window.location.href = 'sahand1/index.html';
 
