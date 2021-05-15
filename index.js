5088930bd9fa6fbf29373ec244306ed6334a10cf
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-15 22:33:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function sahand(){
 var x = document.getElementById("Tag").value;
 
 if (x == 1) {
-    window.location.href = 'sahand.html';
+    window.location.href = 'sahand/index.html';
 }else {
   document.write("Erore");
 }
