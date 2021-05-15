a7594ff9a0059a662cbc0def162e11b8ea3a8124
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-16 00:56:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var x = document.getElementById("Tag").value;
 if (x == "Cl") {
     
 
-  document.forms['contactus'].action='sahand/index.html';
+  document.forms['h'].action='sahand/index.html';
 
 }
 else if (x == "F"){
 
-document.forms['contactus'].action='sahand1/index.html';
+document.forms['h'].action='sahand1/index.html';
 
 
 }
