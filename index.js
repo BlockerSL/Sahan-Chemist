dac8cb64c7e93c9d8d759579be49a3f2e23d8efe
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-15 22:53:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function sahand(){
 var x = document.getElementById("Tag").value;
 
-if (x == 1) {
+if (x == "Cl") {
     window.location.href = 'sahand/index.html';
 }else {
   document.write("Erore");
